88412182ee7a18798afcf395b2ce5fa3b995d6ea
Santiago Pintos <spintos@outlook.es>
2024-04-05 15:57:13 -0300

    Setup Actions
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site:'https://santiagopintos.github.io/',
+    base: 'santiagopintos.github.io',
+});
