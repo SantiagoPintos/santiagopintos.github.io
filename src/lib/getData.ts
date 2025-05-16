6e68b96fd7123082868c501280323c8004d223af
Santiago Pintos <spintos@outlook.es>
2025-05-16 19:46:26 -0300

    feat: setup firebase auth

--- src/lib/getData.ts
+++ src/lib/getData.ts
@@ -1,7 +1,10 @@
 import { db } from "./firebase";
 import { ref, get } from "firebase/database";
+import { getAuth, signInAnonymously } from "firebase/auth";
 
 export const fetchCvData = async () => {
+  const auth = getAuth();
+  await signInAnonymously(auth);
   const snapshot = await get(ref(db, "cv"));
   if (!snapshot.exists()) throw new Error("No CV data found");
   return snapshot.val();
